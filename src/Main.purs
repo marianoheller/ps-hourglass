9e1dcde9650a5d77d62c462443b67f2a9e95ade2
Mariano Heller <mariano.heller@w3americas.com>
2019-02-27 10:12:30 -0300

    fix deps

--- src/Main.purs
+++ src/Main.purs
@@ -1,13 +1,21 @@
 module Main where
 
 import Prelude
-import Control.Monad.Eff (Eff)
+import Effect (Effect)
 import Data.Maybe (Maybe(..))
-import Graphics.Canvas (CANVAS, rect, fillPath, setFillStyle, getContext2D,
+import Graphics.Canvas (CanvasElement , rect, fillPath, setFillStyle, getContext2D,
                         getCanvasElementById)
 import Partial.Unsafe (unsafePartial)
 
-main :: Eff (canvas :: CANVAS) Unit
+
+main :: Effect (canvas :: CanvasElement) Unit
 main = void $ unsafePartial do
   Just canvas <- getCanvasElementById "canvas"
   ctx <- getContext2D canvas
+  setFillStyle "#FF0000" ctx
+  fillPath ctx $ rect ctx
+    { x: 250.0
+    , y: 250.0
+    , w: 100.0
+    , h: 100.0
+    }
