a30e6d570d3b0254d1aeb536e4c84e09ad1abe66
Mariano Heller <mariano.heller@w3americas.com>
2019-03-02 13:15:36 -0300

    add html

--- src/Main.purs
+++ src/Main.purs
@@ -3,19 +3,19 @@ module Main where
 import Prelude
 import Effect (Effect)
 import Data.Maybe (Maybe(..))
-import Graphics.Canvas (CanvasElement , rect, fillPath, setFillStyle, getContext2D,
-                        getCanvasElementById)
+import Graphics.Canvas ({- CanvasElement ,  -}rect, fillPath, setFillStyle, setStrokeStyle,
+                        getContext2D, getCanvasElementById)
 import Partial.Unsafe (unsafePartial)
 
-
-main :: Effect (canvas :: CanvasElement) Unit
+main :: Effect Unit
 main = void $ unsafePartial do
-  Just canvas <- getCanvasElementById "canvas"
+  Just canvas <- getCanvasElementById "targetCanvas"
   ctx <- getContext2D canvas
-  setFillStyle "#FF0000" ctx
+  setFillStyle ctx "red"
+  setStrokeStyle ctx "black"
   fillPath ctx $ rect ctx
     { x: 250.0
     , y: 250.0
-    , w: 100.0
-    , h: 100.0
+    , width: 100.0
+    , height: 100.0
     }
