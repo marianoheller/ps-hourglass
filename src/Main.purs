3905fe6762f4d0d3b2070690f208d0749cd75122
Mariano Heller <mariano.heller@w3americas.com>
2019-03-12 09:57:57 -0300

    wip

--- src/Main.purs
+++ src/Main.purs
@@ -3,6 +3,7 @@ module Main where
 import Prelude
 import Config as Conf
 import Types
+import Math
 import Data.List
 import Data.Int
 import Effect (Effect)
@@ -14,34 +15,37 @@ main :: Effect Unit
 main = do
   mcanvas <- C.getCanvasElementById "targetCanvas"
   case mcanvas of
-    Just canvas -> do
-      configCanvas
-      ctx <- C.getContext2D canvas
-      C.setFillStyle ctx "red"
-      C.setStrokeStyle ctx "black"
-      poses <- createPos Conf.particlesQty
-      (drawParticles ctx Conf.particleRadius) <$> poses
-      pure unit
+    Just canvas -> drawStuff canvas
     Nothing -> pure unit
 
+
+drawStuff :: Effect Unit
+drawStuff canvas =  do
+  configCanvas canvas
+  ctx <- C.getContext2D canvas
+  C.setFillStyle ctx "red"
+  C.setStrokeStyle ctx "black"
+  {- pos <- createInitialPos Conf.particlesQty -}
+  {- e <- drawParticle ctx Conf.particleRadius pos -}
+  e <- drawParticle ctx Conf.particleRadius <$> (createInitialPos Conf.particlesQty)
+  pure unit
 
 configCanvas :: C.CanvasElement -> Effect Unit
 configCanvas canvas = do
   C.setCanvasHeight canvas Conf.canvasHeight
   C.setCanvasWidth canvas Conf.canvasWidth
-  pure unit
 
 
-createPos :: Int -> List TupleXY
-createPos qty = (\n -> { x: n, y: n }) <$> toNumber <$> (range 1 qty)
+createInitialPos :: Int -> List TupleXY
+createInitialPos qty = (\n -> { x: n, y: n }) <<< toNumber <$> (range 1 qty)
 
 
-drawParticles :: C.Context2D -> Number -> TupleXY -> Effect Unit
-drawParticles ctx r p = do
+drawParticle :: C.Context2D -> Number -> TupleXY -> Effect Unit
+drawParticle ctx r p = do
   C.fillPath ctx $ C.arc ctx
     { x: p.x
     , y: p.y
     , radius: r
     , start: 0.0
-    , end: 3.141516
+    , end: pi
     }
